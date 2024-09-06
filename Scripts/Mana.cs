887e7d3dfaa96d7a56d068f43b06449b4361426e
Matthew Nock <thematt.nock@gmail.com>
2024-09-07 00:07:34 +0100

    mana bar

---
+++ Scripts/Mana.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Mana : Node2D
+{
+	[Export]
+	public UI Ui;
+	[Export]
+	public ColorRect manaBar;
+	[Export]
+	public float manaWidth, manaLength;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		manaBar.Size = new Vector2(manaWidth, manaLength);
+		Ui.currentMana = Ui.maxMana;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsKeyPressed(Key.Minus))
+		{
+			Ui.currentMana -= 10;
+		}
+		if (Input.IsKeyPressed(Key.Equal))
+		{
+			Ui.currentMana += 10;
+		}
+		manaBar.Size = new Vector2(Ui.currentMana / Ui.maxMana * manaWidth, manaLength) <= new Vector2(manaWidth, manaLength) ? new Vector2(Ui.currentMana / Ui.maxMana * manaWidth, manaLength) : new Vector2(manaWidth, manaLength);
+		Ui.currentMana = Ui.currentMana <= Ui.maxMana ? Ui.currentMana : Ui.maxMana;
+	}
+}
--- Scripts/UI.cs
+++ Scripts/UI.cs
@@ -7,4 +7,12 @@ public partial class UI : Node2D
 	public float maxHealth, currentHealth;
 	[Export]
 	public int money;
+	public enum classes { Shadow, Fury, Mana, Momentum };
+	[Export]
+	public classes playerClass;
+	//mana class stuff
+	[Export]
+	public float maxMana, currentMana;
+
+
 }
