1ce63e9934a680affa30caa6741de50e8e3a239e
Matthew Nock <thematt.nock@gmail.com>
2024-10-14 20:17:34 +0100

    Jump to closest node

--- Scripts/Shadow_attacks.cs
+++ Scripts/Shadow_attacks.cs
@@ -17,6 +17,34 @@ public partial class Shadow_attacks : Node3D
 	{
 		Position = character.Position;
 	}
+	private Node3D closestShade()
+	{
+		if (ShadeList.Count > 0)
+		{
+			Vector3 closest = new Vector3(Mathf.Inf, Mathf.Inf, Mathf.Inf);
+			Node3D closestNode = null;
+			foreach (Node3D shade in ShadeList)
+			{
+				if (shade.Position.DistanceTo(character.Position) < closest.DistanceTo(character.Position))
+				{
+					closest = shade.Position;
+					closestNode = shade;
+				}
+			}
+			return closestNode;
+		}
+		return null;
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventKey keyEvent && keyEvent.Pressed)
+		{
+			if (Input.IsKeyPressed(Key.Space))
+			{
+				character.Position = closestShade().Position;
+			}
+		}
+	}
 	private void BodyEntered(Node body)
 	{
 		if (body.Name.ToString().Contains("Shade"))
