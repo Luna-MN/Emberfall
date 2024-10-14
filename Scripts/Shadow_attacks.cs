7dfc2058795b61a609a43a334a532b2ad7acc7c4
Matthew Nock <thematt.nock@gmail.com>
2024-10-14 18:51:48 +0100

    Shade collision detection

--- Scripts/Shadow_attacks.cs
+++ Scripts/Shadow_attacks.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Shadow_attacks : Node3D
 {
+	[Export]
+	public CharacterBody3D character;
+	private List<Node3D> ShadeList = new List<Node3D>();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +15,22 @@ public partial class Shadow_attacks : Node3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Position = character.Position;
+	}
+	private void BodyEntered(Node body)
+	{
+		if (body.Name.ToString().Contains("Shade"))
+		{
+			ShadeList.Add((Node3D)body);
+			GD.Print("Shade entered");
+		}
+	}
+	private void BodyExited(Node body)
+	{
+		if (body.Name.ToString().Contains("Shade"))
+		{
+			ShadeList.Remove((Node3D)body);
+			GD.Print("Shade exited");
+		}
 	}
 }
