1bd51a7b4fbf2a8a0755a8b2221e443e91dab02f
Matthew Nock <thematt.nock@gmail.com>
2024-10-27 14:34:56 +0000

    dash when attched

--- Scripts/Shadow_attacks.cs
+++ Scripts/Shadow_attacks.cs
@@ -8,6 +8,11 @@ public partial class Shadow_attacks : Node3D
 	public CharacterBody3D character;
 	private List<Node3D> ShadeList = new List<Node3D>();
 	Timer timer;
+	public bool attached = false, dashed = false;
+	private Godot.Collections.Dictionary rayA;
+	private PhysicsDirectSpaceState3D spaceState;
+	private Vector2 mousePos;
+	private Camera3D cam;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,6 +25,8 @@ public partial class Shadow_attacks : Node3D
 		if (timer != null && timer.IsStopped())
 		{
 			((Movement)character).inhibitMovement = false;
+			attached = false;
+			dashed = false;
 		}
 	}
 	private Node3D closestShade()
@@ -48,6 +55,15 @@ public partial class Shadow_attacks : Node3D
 		timer.Start();
 		((Movement)character).inhibitMovement = true;
 	}
+	public void DashForward()
+	{
+		if (attached && !dashed)
+		{
+			character.Position += (ScreenPointToRay() - character.Position).Normalized() * 10;
+			character.Position = new Vector3(character.Position.X, 0, character.Position.Z);
+			dashed = true;
+		}
+	}
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventKey keyEvent && keyEvent.Pressed)
@@ -55,7 +71,15 @@ public partial class Shadow_attacks : Node3D
 			if (Input.IsKeyPressed(Key.Space))
 			{
 				character.Position = closestShade().Position;
+				attached = true;
 				inhibitMovement();
+			}
+		}
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
+		{
+			if (mouseEvent.ButtonIndex == MouseButton.Right)
+			{
+				DashForward();
 			}
 		}
 	}
@@ -74,5 +98,26 @@ public partial class Shadow_attacks : Node3D
 			ShadeList.Remove((Node3D)body);
 			GD.Print("Shade exited");
 		}
+	}
+	// ray tracing
+	public Vector3 ScreenPointToRay()
+	{
+		// Get the mouse position
+		spaceState = GetWorld3D().DirectSpaceState;
+		mousePos = GetViewport().GetMousePosition();
+		// Get the camera
+		cam = GetTree().Root.GetCamera3D();
+		// Get the ray
+		var rayO = cam.ProjectRayOrigin(mousePos);
+		var rayE = rayO + cam.ProjectRayNormal(mousePos) * 2000;
+		var query = PhysicsRayQueryParameters3D.Create(rayO, rayE);
+		query.CollideWithAreas = true;
+		query.CollisionMask = 1 << 2;
+		// Get the raycast
+		rayA = spaceState.IntersectRay(query);
+		// Return the position of the raycast
+		if (rayA != null)
+			return (Vector3)rayA["position"];
+		return new Vector3(0, 0, 0);
 	}
 }
